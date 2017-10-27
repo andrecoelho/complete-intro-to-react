8bde9aca765a605e682e9243d28de0daa2d688b2
Andre Coelho <acoelho@riskalyze.com>
2017-10-27 11:45:58 -0700

    Test that epic handles errors correctly

--- js/__tests__/epics.spec.js
+++ js/__tests__/epics.spec.js
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { ActionsObservable } from 'redux-observable';
 
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/toArray';
 
 import { getAPIDetails, addAPIData } from '../actionCreators';
@@ -19,16 +20,24 @@ const oitnb = {
   trailer: 'th8WT_pxGqg'
 };
 
+const actionStream = ActionsObservable.of(getAPIDetails(oitnb.imdbID));
+const outputStream = fetchAPIDetails(actionStream, {}, Observable);
+
 test('fetchAPIDetails', () => {
   Observable.ajax = {
     getJSON: () => Observable.of(oitnb)
   };
 
-  const actionStream = ActionsObservable.of(getAPIDetails(oitnb.imdbID));
-  const outputStream = fetchAPIDetails(actionStream, {}, Observable);
-
   outputStream.toArray().subscribe(actions => {
     expect(actions.length).toBe(1);
     expect(actions[0]).toEqual(addAPIData(oitnb));
   });
+});
+
+test('fetchAPIDetails Error', () => {
+  Observable.ajax = {
+    getJSON: () => Observable.throw()
+  };
+
+  outputStream.toArray().subscribe(actions => expect(actions.length).toBe(0));
 });
